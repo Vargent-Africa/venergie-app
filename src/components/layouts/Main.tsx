5a555a6db3d0f9189fc97aa05b933fc8b7606b32
krysceejay <chrischimachijioke@gmail.com>
2025-07-24 14:34:35 +0100

    fix(Cart): Added cart count

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -12,7 +12,7 @@ import * as styled from "./styles/main";
 const MainLayout = () => {
 	const barRef = useRef<HTMLDivElement>(null);
 	const menuRef = useRef<HTMLDivElement>(null);
-	const { showCart } = useCart();
+	const { cart, showCart } = useCart();
 	const { isAuthenticated } = useAuth();
 
 	const handleShowMenu = () => {
@@ -80,7 +80,7 @@ const MainLayout = () => {
 									d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
 								/>
 							</styled.CartIcon>
-							<styled.CartCounter>3</styled.CartCounter>
+							<styled.CartCounter>{cart?.length}</styled.CartCounter>
 						</styled.CartIconWrapper>
 						<styled.MenuIconContainer ref={barRef} onClick={handleShowMenu}>
 							<styled.MenuIcon />
